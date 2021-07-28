7c8738f2682531d44ff9d11970ae9d639b72f000
vinicxxl <75495504+vinicxxl@users.noreply.github.com>
2021-07-28 03:52:17 -0300

    Quinta postagem

--- Js/test/003/js/main.js
+++ Js/test/003/js/main.js
@@ -2,16 +2,12 @@ function trocar(){
     var corpo = window.document.body
     var b = window.document.querySelector('button#botao')
     var p = window.document.querySelector('p')
-    var i = window.document.querySelector('input#atualiza')
 
     corpo.style.background = 'black'
     corpo.style.color = 'white'
    
     b.style.background = 'white'
     b.style.color = 'black'
-   
-    i.style.background = 'white'
-    i.style.color = 'black'
 
     p.style.color = 'white'
     p.innerHTML = 'black'

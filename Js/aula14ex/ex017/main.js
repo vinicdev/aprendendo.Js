e1a1be3de7b9d14871b07f69ff7f7df46094b096
vinicxxl <75495504+vinicxxl@users.noreply.github.com>
2021-08-04 11:57:47 -0300

    ex17
    segunda resolução

--- Js/aula14ex/ex017/main.js
+++ Js/aula14ex/ex017/main.js
@@ -1,17 +1,21 @@
 function gerartabuada(){
-    //recebendo número do input
-    let numTabuada = window.document.querySelector('input#numTabuada')
+    //recebendo número do input e seltab
+    let numTabuada = document.querySelector('input#numTabuada')
+    let tab = document.querySelector('select#seltab')
 
-    //recebendo div res
-    let res = window.document.querySelector('div#res')
-
-    //convertendo número do input
-    let n = Number(numTabuada.value)
-
-    res.innerHTML = `Tabada do <strong>${n}</strong>: <br>`
-
-    for(let c = 0; c <= 10; c++){
-        let s = n * c
-        res.innerHTML += `${n}  x  ${c}  =  <strong>${s}</strong> <br>`
+    //testando se o número é 0 e convertendo número do input
+    if(numTabuada.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(numTabuada.value)
+        //Contador da tabuada
+        res.innerHTML = `Tabada do <strong>${n}</strong>: <br>`
+        tab.innerHTML = ''
+        for(let c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n}  x  ${c}  =  ${n*c} `
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
     }
-}
+    }

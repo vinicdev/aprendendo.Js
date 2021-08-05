2c7533bc0e37789cd833c4d9d5baaf05bef645bf
vinicxxl <75495504+vinicxxl@users.noreply.github.com>
2021-08-05 15:23:50 -0300

    aula 15

---
+++ Js/aula15/forin.js
@@ -0,0 +1,6 @@
+let num = [8,1,7,4,2,9]
+// num.sort()
+
+for(let pos in num){
+    console.log(`A posição ${pos} tem o valor: ${num[pos]}`)
+}
---
+++ Js/aula15/vetornatela.js
@@ -0,0 +1,5 @@
+let num = [8,1,7,4,2,9]
+
+for(let pos = 0; pos < num.length; pos++){
+    console.log(`A posição ${pos} tem o valor: ${num[pos]}`)
+}

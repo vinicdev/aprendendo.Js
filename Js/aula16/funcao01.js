3bcdfe6ec3bbe316fd3107186dceda2259e60309
vinicxxl <75495504+vinicxxl@users.noreply.github.com>
2021-08-09 22:08:00 -0300

    Aula 16

---
+++ Js/aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimp(n){
+    if (n%2 == 0){
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+//  Primeira forma 
+// let res = parimp(5)
+// console.log(res)
+
+//  Segunda forma
+console.log(parimp(4))
